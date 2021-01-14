518b026511a05e9a9358e5cdb1907aa95ccb1f1b
WILL MEIER <willmeierart@gmail.com>
2021-01-13 20:51:28 -0700

    Fix analytics, add metas

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -49,21 +49,7 @@ const words = [
 ]
 
 class Application extends App {
-	// static async getInitialProps ({ Component, ctx }) {
-	// 	const pageProps = Component.getInitialProps
-	// 		? await Component.getInitialProps({ ...ctx })
-	// 		: {}
-
-	// 	return { pageProps }
-	// }
-
 	componentDidMount () {
-		window.dataLayer = window.dataLayer || []
-		function gtag () {
-			window.dataLayer.push(arguments)
-		}
-		gtag('config', 'G-GZ7H5KZ3H0')
-
 		let activeIndex = 0
 		setInterval(() => {
 			this.setState({ activeTitleWord: words[activeIndex] })
@@ -73,6 +59,9 @@ class Application extends App {
 				activeIndex += 1
 			}
 		}, 500)
+
+		console.log('%cMade with 💖 with next and three by Will Meier 2021', 'color: yellow; font-size: 20px')
+		console.log('...under construction')
 	}
 
 	state = {
@@ -86,16 +75,26 @@ class Application extends App {
 			router,
 		} = this.props
 
-		console.log('Made with 💖 with next.js and react-three-fiber by Will Meier 2021 (under construction...)')
-
 		return (
 			<Layout router={router}>
 				<Head>
 					<script async src="https://www.googletagmanager.com/gtag/js?id=G-GZ7H5KZ3H0" />
+					<script dangerouslySetInnerHTML={{
+						__html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+							gtag('config', '[G-GZ7H5KZ3H0]');
+						`
+					}} />
 					<meta property="og:type" content="website" />
 					<meta property="og:site_name" content="will meier" />
 					<meta property="og:locale" content="en_US" />
-					<meta name="twitter:card" content="summary" />
+					<meta property="og:title" content="will meier" />
+					<meta property="og:description" content="a super cool website" />
+					<meta property="og:image" content="/static/images/og.jpg" />
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta property="og:url" content="https://willmeier.cool" />
 					<link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
 					<title>{this.state.activeTitleWord}</title>
 				</Head>
